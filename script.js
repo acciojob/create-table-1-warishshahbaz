804fdbb92182591e7b6262a6fbdedacc100a350d
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-14 00:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function insert_Row() {
     // Get the reference to the table
     var table = document.getElementById("sampleTable");
 
-    // Insert a new row at the last position
-    var newRow = table.insertRow(-1);
+    // Insert a new row at the beginning of the table
+    var newRow = table.insertRow(0);
 
-    // Insert cells in the new row
+    // Insert two cells in the new row
     var cell1 = newRow.insertCell(0);
     var cell2 = newRow.insertCell(1);
 
-    // Set the values for the cells
+    // Set the content of the new cells
     cell1.innerHTML = "New Cell1";
     cell2.innerHTML = "New Cell2";
 }
